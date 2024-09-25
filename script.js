0a175bb5079c7f24d6c2e02780bcdde3ed687947
Nicholas <43795440+Loxodograph@users.noreply.github.com>
2024-09-25 14:48:18 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,18 +3,28 @@ let container = document.querySelector('.container');
 const button = document.getElementById('submit-btn');
 const slider = document.getElementById("myRange");
 const sliderValue = document.getElementById("slider-value");
+document.addEventListener("touchstart", function(){}, true);
 
-slider.addEventListener("onclick", updateSliderText);
-slider.addEventListener("click", updateSliderText);
-slider.addEventListener("change", updateSliderText);
+
+// slider.addEventListener("onclick", updateSliderText);
+// slider.addEventListener("click", updateSliderText);
+// slider.addEventListener("change", updateSliderText);
+
+//let checkbox change to random color
+let checkbox = document.querySelector("input[name=switch]");
+
+//let slider input update
+slider.addEventListener("input", updateSliderText);
 sliderValue.style.color = `black`;
 
 function generateDivs() {
   
-    gridWidth = gridSize(); 
-    color = random_rgb();
+    let gridWidth = gridSize(); 
+    let color = random_rgb();
     let i = 0;
+    checkbox.addEventListener('change', colorSwitcher);
     //create the grid spans
+    
     while (i < gridWidth ** 2){
         let box = document.createElement('div');
         box.innerText = ""
@@ -23,29 +33,36 @@ function generateDivs() {
         box.style.width = `${40 / gridWidth}rem`;
         box.style.height = `${40 / gridWidth}rem`;
         container.appendChild(box);
-        box.addEventListener("mouseover", function (){
-        box.style.background = color;
-        })
+        box.style.opacity = .3;
 
-        box.addEventListener("touchmove", function() {
-          box.style.background = color;
-          console.log('mousemove')
-        });
+        //add color to mousover event
+        box.addEventListener("mouseover", function (){
+          
+        box.style.background = color;
+
+        if (box.style.opacity <= 0.9) {
+          box.style.opacity = +box.style.opacity + 0.1;
+          // +e.style.opacity to convert opacity from string to number
+        }
+      
+
+        //add color randomizer to switch
         
-        box.addEventListener("touchstart", function() {
-          box.style.background = color;
-        });
+        })
         i++
     }
      
   }
 
+  //generate css for random rgb
 function random_rgb() {
   let red = Math.floor(Math.random() * 256);
   let green = Math.floor(Math.random() * 256);
   let blue = Math.floor(Math.random() * 256);
   return `rgb(${red}, ${green}, ${blue})`
 }
+
+//receive gridsize from slider
 
 function gridSize() {
   const value = document.getElementById("myRange").value;
@@ -72,4 +89,25 @@ function removeElementsByClass(className) {
 function updateSliderText() {
   let value = document.getElementById("myRange").value;
   sliderValue.innerText = `${value} x ${value}`;
+}
+
+
+//function for generating random color
+function colorSwitcher() {
+  const gridBox = document.querySelectorAll(".grid") ;
+  const gridBoxArray = [...gridBox]
+  const staticColor = random_rgb();
+  if (this.checked) {
+    gridBoxArray.forEach(div => {
+      div.addEventListener("mouseover", function (){
+        div.style.background = `${random_rgb()}`;
+        })
+      });
+  } else {
+    gridBoxArray.forEach(div => {
+      div.addEventListener("mouseover", function (){
+        div.style.background = staticColor;
+        })
+      });
+  }
 }
